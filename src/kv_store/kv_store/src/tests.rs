e03f456fed09f950779bc128792700563f272820
Sinval Vieira <sinvalneto01@gmail.com>
2019-02-26 21:41:21 +0100

    Refactoring test case to assert /api response body

--- src/kv_store/kv_store/src/tests.rs
+++ src/kv_store/kv_store/src/tests.rs
@@ -3,16 +3,15 @@
 mod tests {
     use rocket::local::Client;
     use crate::get_rocket;
+    use rocket::http::Status;
 
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
-        println!("Hello tests");
-
         let rocket = get_rocket();
         let client = Client::new(rocket).expect("valid rocket instance");
         let req = client.get("/api");
-        let response = req.dispatch();
-        println!("{:?}", response);
+        let mut response = req.dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string(), Some("{}".into()));
     }
 }
