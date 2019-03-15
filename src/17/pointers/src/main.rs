1fb00ecae0c529c00a6f090f9c1b25ac01ccbee7
Sinval Vieira <sinvalneto01@gmail.com>
2019-03-15 23:10:40 +0100

    Added drop trait example

--- src/17/pointers/src/main.rs
+++ src/17/pointers/src/main.rs
@@ -10,4 +10,21 @@ fn main() {
     assert_eq!(5, x); // True
     // assert_eq!(5, y); // this is different, because y value is acctually a &{integer} (reference to a reference to an integer)
     assert_eq!(5, *y); // True, we are dereferencing y, so we get the value at the and of the "rabbit hole" of references
+
+
+    let c = CustomSmartPointer { data: String::from("my stuff") };
+    let d = CustomSmartPointer { data: String::from("other stuff") };
+    println!("CustomSmartPointers created.");
+    // this is the last line of the main() function, c and d will go out of scope and our custom drop function will be called
+}
+
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
 }
