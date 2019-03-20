68b2a171a628df0d8bc1290eda678b89b92b0cd9
Sinval Vieira <sinvalneto01@gmail.com>
2019-03-20 18:15:22 +0100

    Refactored the concurrent version

--- src/18/array_sum/src/main.rs
+++ src/18/array_sum/src/main.rs
@@ -3,7 +3,6 @@ mod tests;
 
 use std::time::{Instant};
 
-
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::vec::Vec;
@@ -31,34 +30,44 @@ impl Sum {
         return result;
     }
 
-    fn sum_concurrent(self) -> std::vec::Vec<thread::JoinHandle<()>> {
-        let _result = 0;
-        let array_len = self.array.len();
-        let mut handles = vec![];
-        // let mutex_array = Arc::new(Mutex::new(self.array));
+    fn sum_concurrent(self) -> i32 {
+        return 0;
+    }
+
+    fn get_thread_pool(self, number_of_threads: i32, slice:i32) -> std::vec::Vec<thread::JoinHandle<()>> {
+        let mut threads = vec![];
         let mutex = Arc::new(Mutex::new(0));
 
         let mut k = 0;
-        for i in 1..9 {
-            let slice = array_len/8;
-            let offset = (slice * i) - 1;
+        for i in 0..number_of_threads {
+            let offset = slice * (i + 1);
 
             let thread_array = self.array.clone();
-            let c_mutex = Arc::clone(&mutex);
-            // let a_mutex = Arc::clone(&mutex_array);
-            let t1 = thread::spawn(move || {
-                // let array = a_mutex.lock().unwrap();
+            let counter_mutex = Arc::clone(&mutex);
+            let t = thread::spawn(move || {
                 let mut thread_internal_count = 0;
                 for j in k..offset {
-                    thread_internal_count += thread_array[j];
+                    thread_internal_count += thread_array[j as usize];
                 }
-                let mut count = c_mutex.lock().unwrap();
+                let mut count = counter_mutex.lock().unwrap();
                 *count += thread_internal_count;
             });
             k = offset + 1;
-            handles.push(t1);
+            threads.push(t);
         }
-        return handles;
+        return threads;
+    }
+
+    fn get_sum_thread_pool(self) -> std::vec::Vec<thread::JoinHandle<()>> {
+        let mut number_of_threads: i32 = 10;
+        let array_len: i32 = self.array.len() as i32;
+
+        if self.array.len() % 10 != 0 {
+            number_of_threads = 1;
+        }
+        let slice: i32 = array_len/number_of_threads;
+
+        return self.get_thread_pool(number_of_threads, slice);
     }
 }
 
@@ -67,9 +76,9 @@ fn benchmark1() {
     let bench_name = "benchmark conc1";
     let mut sum = Sum::new();
     sum.populate_array();
-    let handles = sum.sum_concurrent();
+    let thread_pool = sum.get_sum_thread_pool();
     let now = Instant::now();
-    for handle in handles {
+    for handle in thread_pool {
         handle.join().unwrap();
     }
     let elapsed = now.elapsed().as_nanos();
@@ -81,9 +90,9 @@ fn benchmark2 () {
     let mut sum = Sum::new();
     sum.populate_array();
     let now = Instant::now();
-    sum.sum_iter();
+    let result = sum.sum_iter();
     let elapsed = now.elapsed().as_nanos();
-    println!("{}: {}", bench_name, elapsed);
+    println!("{}:, result:{}, elapsed time:{}", bench_name, result, elapsed);
 }
 
 pub fn main() {
