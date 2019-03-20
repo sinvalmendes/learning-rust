6b70d5466bba4af8e270d3d0eb482fe834d88c93
Sinval Vieira <sinvalneto01@gmail.com>
2019-03-20 21:33:45 +0100

    Added multiple borrowed example

--- src/7/reference_and_borrowing/src/main.rs
+++ src/7/reference_and_borrowing/src/main.rs
@@ -13,6 +13,13 @@ fn main() {
     println!("counter: {}, after first mutation", counter);
     increment_mutable_counter_reference(&mut counter); // you can borrow the mutable reference as much as you want
     println!("counter: {}, after second mutation", counter);
+
+    let mut counter_to_be_borrowed_twice = 0;
+    println!("counter_to_be_borrowed_twice: {}, before mutation", counter_to_be_borrowed_twice);
+    borrow_to_another_function(&mut counter_to_be_borrowed_twice);
+    println!("counter_to_be_borrowed_twice: {}, after mutation", counter_to_be_borrowed_twice);
+
+
 }
 
 fn calculate_length(s_pointer: &String) -> usize { // &String means the function want a pointer/reference to a String value in the Heap
@@ -27,4 +34,9 @@ fn borrowing_to_mutate(s_mutable_pointer: &mut String) {
 
 fn increment_mutable_counter_reference(counter: &mut i32) {
     *counter += 1; // counter is incremented and won't die at the end of this function
+}
+
+fn borrow_to_another_function(counter: &mut i32) {
+    increment_mutable_counter_reference(counter); // borrowing the already borrowd mutable reference
+    // counter won't die at the end of this function because this function doesn't own it, just have it borrowed as mutable
 }
