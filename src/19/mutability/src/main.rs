d50bc92d3fc61a87ae1b6b09956f7944a2fee18f
Sinval Vieira <sinvalneto01@gmail.com>
2019-03-22 21:13:02 +0100

    Added basic mutability examples

--- src/19/mutability/src/main.rs
+++ src/19/mutability/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = "value";
+    println!("x:{}", x);
+    x = "mutated";
+    println!("x after mutation: {}", x);
+
+    let y = &mut x;
+    println!("y: {}", y);
+    // let z = &mut x; // this won't compile, there can only be one borrowed value, and right now y is the only one
+
+    *y = "mutated by y";
+    println!("y after mutation: {}", y);
+    println!("x after mutation: {}", x);
+
+    x = "mutated by x";
+    println!("x after mutation: {}", x);
+    // y is a borrow, and cannot be used anymore, x is the only valid reference now
+    // there can only be one mutable borrowed reference to a value, but y can borrow x again...
 }
