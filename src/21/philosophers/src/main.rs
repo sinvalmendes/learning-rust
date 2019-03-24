fa2bb8c958f8bde7fa897de18d3a9301460144d0
Sinval Vieira <sinvalneto01@gmail.com>
2019-03-24 18:53:22 +0100

    Added logic of reading forks mutex

--- src/21/philosophers/src/main.rs
+++ src/21/philosophers/src/main.rs
@@ -32,10 +32,34 @@ fn main() {
 }
 
 
-fn create_philosopher_thread(mutex: &mut Arc<Mutex<Vec<&'static str>>>, name: &'static str, left_fork: u8, right_fork: u8) -> thread::JoinHandle<()> {
-    let forks_clone = Arc::clone(&mutex);
+fn create_philosopher_thread(mutex: &mut Arc<Mutex<Vec<&'static str>>>, name: &'static str, left_fork: usize, right_fork: usize) -> thread::JoinHandle<()> {
+    let forks = Arc::clone(&mutex);
+    let needed_left_fork = format!("fork{}", left_fork);
+    let needed_right_fork = format!("fork{}", right_fork);
+
     let t = thread::spawn(move || {
         println!("P{} needs left fork '{}' and right fork '{}'.", name, left_fork, right_fork);
+        loop {
+            let mut locked_forks = forks.lock().unwrap();
+            let philosopher_left_fork = locked_forks[left_fork];
+            let philosopher_right_fork = locked_forks[right_fork];
+
+            if philosopher_left_fork == needed_left_fork{
+                println!("P{} got left_fork: {}", name, philosopher_left_fork);
+            } else {
+                println!("P{} didn't get left_fork: {}", name, "other thing");
+            }
+
+            if philosopher_right_fork == needed_right_fork{
+                println!("P{} got right_fork: {}", name, philosopher_right_fork);
+            } else {
+                println!("P{} didn't get right_fork: {}", name, "other thing");
+            }
+
+            println!("P{} current status: right_fork '{}', right_fork '{}'", name, philosopher_left_fork, philosopher_right_fork);
+            break;
+        }
+    
     });
     return t;
 }
