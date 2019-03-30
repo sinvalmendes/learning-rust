ab548d7d082cf5db831d584870e2600198981d40
Sinval Vieira <sinvalneto01@gmail.com>
2019-03-30 10:55:13 +0100

    Added new project about Copy trait

--- src/14/clojures/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let clojure = |a| { // defining a clojure called "clojure" that receives parameter "a"
-        println!("{}", a);
-    };
-
-    let b = String::from("this is b");
-    clojure(b);
-}
