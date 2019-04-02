b87212ba4024020ca4ebc2db518e1243b52626a5
Sinval Vieira <sinvalneto01@gmail.com>
2019-04-02 22:23:06 +0200

    Added example showing how to use functions receiving other functions as parameters

--- src/14/closures/src/main.rs
+++ src/14/closures/src/main.rs
@@ -5,4 +5,16 @@ fn main() {
 
     let b = String::from("this is b");
     closure(b);
+
+    call_function(callback, "this is the value of 'text' parameter comming from main calling 'call_function' passing 'callback' as a parameter");
 }
+
+fn call_function(f: fn(&str), f_param: &str) {
+    f(f_param);
+}
+
+fn callback(text: &str) {
+    println!("this is advanced function usage! {}", text);
+}
+
+
