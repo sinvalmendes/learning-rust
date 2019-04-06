3ab7453ee32c5a3a26b50ba95ed35d7f2a3e66e9
Sinval Vieira <sinvalneto01@gmail.com>
2019-04-06 19:17:03 +0200

    Added Arc::Barrier to syncronize the LIFT OFF across all the threads

--- src/23/rocket_preparation/src/main.rs
+++ src/23/rocket_preparation/src/main.rs
@@ -1,7 +1,7 @@
 extern crate rand;
 
 use rand::Rng;
-use std::sync::{Arc, Mutex};
+use std::sync::{Arc, Barrier, Mutex};
 use std::vec::Vec;
 use std::{thread, time};
 
@@ -15,30 +15,38 @@ fn main() {
     println!("Hello, Rocket Preparation problem!");
     let resources = vec![FUEL, OXI, FUEL, ASTRO, ASTRO, OXI, OXI, FUEL, ASTRO]; // concrete vector to simplify implementation for now
     println!("Resources: {:?}", resources);
+    let lift_off_arc = Arc::new(Barrier::new(4));
 
     let mut mutex: Arc<Mutex<Vec<&'static str>>> = Arc::new(Mutex::new(resources));
     let mut thread_pool = vec![];
 
-    let r0 = create_rocket_thread(&mut mutex, "STS-1");
+    let lift_off0 = Arc::clone(&lift_off_arc);
+    let r0 = create_rocket_thread(&mut mutex, "STS-1", lift_off0);
     thread_pool.push(r0);
 
-    let r1 = create_rocket_thread(&mut mutex, "Apollo 11");
+    let lift_off1 = Arc::clone(&lift_off_arc);
+    let r1 = create_rocket_thread(&mut mutex, "Apollo 11", lift_off1);
     thread_pool.push(r1);
 
-    let r2 = create_rocket_thread(&mut mutex, "STS-66");
+    let lift_off2 = Arc::clone(&lift_off_arc);
+    let r2 = create_rocket_thread(&mut mutex, "STS-66", lift_off2);
     thread_pool.push(r2);
 
+    sleep("Main", 10000, 10001);
+    lift_off_arc.wait();
+
     for thread in thread_pool {
-        thread.join();
+        thread.join().unwrap();
     }
+
 }
 
-fn create_producer_thread() {}
+fn control_room_thread() {
+    
+}
 
 fn create_rocket_thread(
-    mutex: &mut Arc<Mutex<Vec<&'static str>>>,
-    name: &'static str,
-) -> thread::JoinHandle<()> {
+    mutex: &mut Arc<Mutex<Vec<&'static str>>>, name: &'static str, lift_off: Arc<Barrier>) -> thread::JoinHandle<()> {
     let mut produced_resources = Arc::clone(&mutex);
 
     let t = thread::spawn(move || {
@@ -60,15 +68,17 @@ fn create_rocket_thread(
                 }
                 i += 1;
             }
-            sleep(name);
+            sleep(name, 1000, 2000);
         }
+        lift_off.wait();
+        println!("Rocket: {} LIFT OFF", name);
     });
     return t;
 }
 
-fn sleep(thread_name: &'static str) {
+fn sleep(thread_name: &'static str, min: u64, max: u64) {
     let mut rng = rand::thread_rng();
-    let random = rng.gen_range(1000, 5000);
+    let random = rng.gen_range(min, max);
     println!("{} sleeping for: {} milliseconds", thread_name, random);
     let sleep = time::Duration::from_millis(random);
     thread::sleep(sleep);
