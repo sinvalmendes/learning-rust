7852bd7be28d84cb14a4e1d6bb1d3b90da510c50
Sinval Vieira <sinvalneto01@gmail.com>
2019-05-30 17:35:34 +0200

    added description of problem

--- src/problems/03/reverse_integer/src/main.rs
+++ src/problems/03/reverse_integer/src/main.rs
@@ -1,3 +1,14 @@
+// Given a 32-bit signed integer, reverse digits of an integer.
+
+// Example 1:
+
+// Input: 123
+// Output: 321
+
+// Example 2:
+// Input: -123
+// Output: -321
+
 fn main() {
     println!("Hello, world!");
 }
