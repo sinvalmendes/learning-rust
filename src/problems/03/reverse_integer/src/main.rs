d77e39047a25443715e33064ef7caf47e6108844
Sinval Vieira <sinvalneto01@gmail.com>
2019-06-03 19:31:55 +0200

    Added more test cases and updated solution to use i64 - reverse integer problem

--- src/problems/03/reverse_integer/src/main.rs
+++ src/problems/03/reverse_integer/src/main.rs
@@ -15,20 +15,26 @@ fn main() {
 
     let result = Solution::reverse(-123);
     assert_eq!(-321, result);
+
+    let result = Solution::reverse(120);
+    assert_eq!(21, result);
+    
+    let result = Solution::reverse(1534236469);
+    assert_eq!(9646324351, result);
 }
 
 struct Solution {}
 
 impl Solution {
-    pub fn reverse(x: i32) -> i32 {
+    pub fn reverse(x: i32) -> i64 {
         let is_negative = x < 0;
         let string = x.to_string();
         let mut reverse_string = string.chars().rev().collect::<String>();
         if is_negative {
             reverse_string.truncate(reverse_string.len() - 1);
-            return reverse_string.parse::<i32>().unwrap() * -1;
+            return reverse_string.parse::<i64>().unwrap() * -1;
         }
 
-        return reverse_string.parse::<i32>().unwrap();
+        return reverse_string.parse::<i64>().unwrap();
     }
 }
