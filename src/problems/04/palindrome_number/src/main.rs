62bcc38a593ac394910ecf0575041817d8bc379e
Sinval Vieira Mendes Neto <sinvalneto01@gmail.com>
2019-06-05 16:15:44 +0200

    Added solution of palindrome integer

--- src/problems/04/palindrome_number/src/main.rs
+++ src/problems/04/palindrome_number/src/main.rs
@@ -17,5 +17,22 @@
 // Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 
 fn main() {
-    println!("Hello, world!");
+    let result = Solution::is_palindrome(123);
+    assert_eq!(false, result);
+
+    let result = Solution::is_palindrome(121);
+    assert_eq!(true, result);
+
+    let result = Solution::is_palindrome(-121);
+    assert_eq!(false, result);
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        let string = x.to_string();
+        let reverse_string = string.chars().rev().collect::<String>();
+        return reverse_string == string;
+    }
 }
