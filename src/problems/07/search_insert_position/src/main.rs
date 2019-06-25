34aa83dba8bed9512a14c6eccf72e4c3550b968e
Sinval Vieira Mendes Neto <sinvalneto01@gmail.com>
2019-06-25 14:53:32 +0200

    Added iterative solution to search insert position problem O(n)

--- src/problems/07/search_insert_position/src/main.rs
+++ src/problems/07/search_insert_position/src/main.rs
@@ -1,4 +1,5 @@
-// Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// Given a sorted array and a target value, return the index if the target is found. 
+// If not, return the index where it would be if it were inserted in order.
 // You may assume no duplicates in the array.
 
 // Example 1:
@@ -18,18 +19,37 @@
 // Output: 0
 
 fn main() {
-    println!("Hello, world!");
 
     let nums = vec![1,3,5,6];
     let result = Solution::search_insert(nums, 5);
     assert_eq!(2, result);
+
+    let nums = vec![1,3,5,6];
+    let result = Solution::search_insert(nums, 2);
+    assert_eq!(1, result);
+
+    let nums = vec![1,3,5,6];
+    let result = Solution::search_insert(nums, 7);
+    assert_eq!(4, result);
+
+    let nums = vec![1,3,5,6];
+    let result = Solution::search_insert(nums, 0);
+    assert_eq!(0, result);
 }
 
 struct Solution {}
 
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        
-        return 0;
+        let mut count = 0;
+        for num in nums {
+            if target == num {
+                return count
+            } else if target < num {
+                return count
+            }
+            count += 1;
+        }
+        return count;
     }
 }
