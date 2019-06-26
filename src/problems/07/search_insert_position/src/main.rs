90496035aacc1f024e31287b7c8a4c3c03f40887
Sinval Vieira Mendes Neto <sinvalneto01@gmail.com>
2019-06-26 16:30:09 +0200

    Added a version of solution using binary search, not fully completed

--- src/problems/07/search_insert_position/src/main.rs
+++ src/problems/07/search_insert_position/src/main.rs
@@ -35,6 +35,24 @@ fn main() {
     let nums = vec![1,3,5,6];
     let result = Solution::search_insert(nums, 0);
     assert_eq!(0, result);
+
+
+
+    let nums = vec![1,3,5,6];
+    let result = Solution::binary_search_insert(nums, 5);
+    assert_eq!(2, result);
+
+    let nums = vec![1,3,5,6];
+    let result = Solution::binary_search_insert(nums, 2);
+    assert_eq!(1, result);
+
+    let nums = vec![1,3,5,6];
+    let result = Solution::binary_search_insert(nums, 7);
+    assert_eq!(4, result);
+
+    let nums = vec![1,3,5,6];
+    let result = Solution::binary_search_insert(nums, 0);
+    assert_eq!(0, result);
 }
 
 struct Solution {}
@@ -51,5 +69,44 @@ impl Solution {
             count += 1;
         }
         return count;
+    }
+
+    pub fn binary_search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        let len = nums.len() as i32;
+        let result = Solution::recursive_search(nums, target, 0, len - 1);
+        return result;
+    }
+
+    pub fn recursive_search(nums: Vec<i32>, target: i32, begin: i32, end: i32) -> i32 {
+        println!("\n+++++++++++++");
+        println!("{} {}", begin, end);
+        let middle = (begin + end) / 2;
+        println!("| begin {:?} | end {:?} | middle {:?}|", begin, end, middle);
+        println!("|{:?} | target {:?} |", nums, target);
+        if middle == begin { 
+            return middle;
+        }
+        let mut current_value: i32 = 999999999;
+
+        match nums.get(middle as usize) {
+                Some(x) => {
+                    current_value = *x;
+                }
+                None => {
+                    panic!("Something is wrong.");
+                }
+        };
+
+
+
+        if current_value > target {
+            let new_end = middle - 1;
+            println!("{} > {}, {}:{}", current_value, target, begin, new_end);
+            return Solution::recursive_search(nums, target, begin, new_end);
+        } else {
+            println!("{} <= {}, {}:{}", current_value, target, middle, end);
+            return Solution::recursive_search(nums, target, middle, end);
+        }
+
     }
 }
