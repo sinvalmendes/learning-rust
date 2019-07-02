d3f43b7ed33a07775bffd1443d46581aa5aee1fb
Sinval Vieira <sinvalneto01@gmail.com>
2019-07-02 20:24:01 +0200

    Added traversal logic for backtracking permutation solution

--- src/problems/08/permutations/src/main.rs
+++ src/problems/08/permutations/src/main.rs
@@ -27,7 +27,20 @@ struct Solution {}
 
 impl Solution {
     pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        
+        let mut result = vec![];
+        let mut count = 0;
+        for num in &nums {
+            let mut nums_clone = nums.clone();
+            let head_num = nums_clone.remove(count);
+            println!("\n===head_num:{:?}, nums:{:?}", head_num, nums_clone);
+            Solution::recursion(&mut result, head_num, nums_clone);
+
+            count += 1;
+        }
         return vec![vec![1,2,3]];
+    }
+
+    pub fn recursion(result: &mut Vec<Vec<i32>>, head_num: i32, nums: Vec<i32>) {
+        println!("result:{:?}, head_num:{:?}, nums:{:?}", result, head_num, nums);
     }
 }
