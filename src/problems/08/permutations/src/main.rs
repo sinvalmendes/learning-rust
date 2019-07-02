feadbeafa6bbd546a894fe86784e1118a91be6e3
Sinval Vieira <sinvalneto01@gmail.com>
2019-07-02 19:57:45 +0200

    Added template for solution and first test scenario

--- src/problems/08/permutations/src/main.rs
+++ src/problems/08/permutations/src/main.rs
@@ -14,5 +14,20 @@
 // ]
 
 fn main() {
-    println!("Hello, world!");
+    let nums = vec![1,2,3];
+    let expected = vec![
+        vec![1,2,3], vec![1,3,2], vec![2,1,3],
+        vec![2,3,1], vec![3,1,2], vec![3,2,1],
+    ];
+    let result = Solution::permute(nums);
+    assert_eq!(expected, result);
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
+        
+        return vec![vec![1,2,3]];
+    }
 }
