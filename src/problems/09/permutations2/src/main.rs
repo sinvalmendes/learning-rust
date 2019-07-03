c33b9934d67287501383b5307dcaedf9bf0609fd
Sinval Vieira Mendes Neto <sinvalneto01@gmail.com>
2019-07-03 19:40:41 +0200

    Added description and solution template for permutations2 problem

--- src/problems/09/permutations2/src/main.rs
+++ src/problems/09/permutations2/src/main.rs
@@ -1,3 +1,29 @@
+// Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+
+// Example:
+
+// Input: [1,1,2]
+// Output:
+// [
+//   [1,1,2],
+//   [1,2,1],
+//   [2,1,1]
+// ]
+
 fn main() {
-    println!("Hello, world!");
+    let nums = vec![1,1,2];
+    let expected = vec![
+        vec![1,1,2], vec![1,2,1], vec![2,1,1],
+    ];
+    let result = Solution::permute_unique(nums);
+    assert_eq!(expected, result);
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn permute_unique(nums: Vec<i32>) -> Vec<Vec<i32>> {
+
+        return vec![vec![0,0,0]];
+    }
 }
