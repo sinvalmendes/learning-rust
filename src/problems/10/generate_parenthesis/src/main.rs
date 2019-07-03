b56fc76e60bd72dc91404e02111f5ea8647ba97c
Sinval Vieira Mendes Neto <sinvalneto01@gmail.com>
2019-07-03 20:52:36 +0200

    Added solution template for generate_parenthesis problem

--- src/problems/10/generate_parenthesis/src/main.rs
+++ src/problems/10/generate_parenthesis/src/main.rs
@@ -1,3 +1,30 @@
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+// For example, given n = 3, a solution set is:
+
+// [
+//   "((()))",
+//   "(()())",
+//   "(())()",
+//   "()(())",
+//   "()()()"
+// ]
+
 fn main() {
-    println!("Hello, world!");
+    let n = 3;
+    let expected = vec![
+        String::from("((()))"), String::from("(()())"), String::from("(())()"), 
+        String::from("()(())"), String::from("()()()")
+    ];
+    let result = Solution::generate_parenthesis(n);
+    assert_eq!(expected, result);
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        
+        return vec![String::from("")];
+    }
 }
