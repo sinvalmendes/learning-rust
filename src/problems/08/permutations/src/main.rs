a6d34b8f50d9e500c26cf7e13f89ecfc877db94d
Sinval Vieira <sinvalneto01@gmail.com>
2019-07-05 22:06:47 +0200

    Changed unecessary for each to use while loop

--- src/problems/08/permutations/src/main.rs
+++ src/problems/08/permutations/src/main.rs
@@ -14,16 +14,20 @@
 // ]
 
 fn main() {
-    let nums = vec![1,2,3];
+    let nums = vec![1, 2, 3];
     let expected = vec![
-        vec![1,2,3], vec![1,3,2], vec![2,1,3],
-        vec![2,3,1], vec![3,1,2], vec![3,2,1],
+        vec![1, 2, 3],
+        vec![1, 3, 2],
+        vec![2, 1, 3],
+        vec![2, 3, 1],
+        vec![3, 1, 2],
+        vec![3, 2, 1],
     ];
     let result = Solution::permute(nums);
     assert_eq!(expected, result);
 
-    let nums = vec![1,2];
-    let expected = vec![vec![1,2], vec![2,1]];
+    let nums = vec![1, 2];
+    let expected = vec![vec![1, 2], vec![2, 1]];
     let result = Solution::permute(nums);
     assert_eq!(expected, result);
 }
@@ -34,11 +38,14 @@ impl Solution {
     pub fn permute(nums: Vec<i32>) -> Vec<Vec<i32>> {
         let mut result = vec![];
         let mut count = 0;
-        for num in &nums {
+        while count < nums.len() {
             let mut available_choices = nums.clone();
             let head_num = available_choices.remove(count);
             let preselected_nums = vec![head_num];
-            println!("\n===preselected_nums:{:?}, available_choices:{:?}", preselected_nums, available_choices);
+            println!(
+                "\n===preselected_nums:{:?}, available_choices:{:?}",
+                preselected_nums, available_choices
+            );
             Solution::recursion(&mut result, preselected_nums, available_choices);
 
             count += 1;
@@ -47,8 +54,15 @@ impl Solution {
         return result;
     }
 
-    pub fn recursion(result: &mut Vec<Vec<i32>>, preselected_nums: Vec<i32>, available_choices: Vec<i32>) {
-        println!("result:{:?}, preselected_nums:{:?}, available_choices:{:?}", result, preselected_nums, available_choices);
+    pub fn recursion(
+        result: &mut Vec<Vec<i32>>,
+        preselected_nums: Vec<i32>,
+        available_choices: Vec<i32>,
+    ) {
+        println!(
+            "result:{:?}, preselected_nums:{:?}, available_choices:{:?}",
+            result, preselected_nums, available_choices
+        );
 
         if available_choices.len() == 0 {
             println!("adding {:?} to result", preselected_nums);
@@ -57,7 +71,7 @@ impl Solution {
         }
 
         let mut count = 0;
-        for num in &available_choices {
+        while count < available_choices.len() {
             let mut available_choices_clone = available_choices.clone();
             let selected_num = available_choices_clone.remove(count);
 
