7cfdfd7d47942f9fe5cfd8fa1d8ffb428395cbbe
Sinval Vieira <sinvalneto01@gmail.com>
2019-07-29 22:14:59 +0200

    Added more routes to the hello world app

--- src/26/actix_hello_world/src/main.rs
+++ src/26/actix_hello_world/src/main.rs
@@ -1,11 +1,24 @@
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 fn index(info: web::Path<(u32, String)>) -> impl Responder {
     format!("Hello {}! id:{}", info.1, info.0)
 }
 
+fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+fn index3() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
+
 fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(web::resource("/{id}/{name}/index.html").to(index)))
-        .bind("0.0.0.0:8080")?
-        .run()
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/{id}/{name}/index.html").to(index))
+            .service(web::resource("/index2").to(index2))
+            .service(web::resource("/index3").to(index3))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
 }
