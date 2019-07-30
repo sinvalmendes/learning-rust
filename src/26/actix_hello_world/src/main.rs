4e410777d8e5e3f34acdda6bf0ff2c35547353ea
Sinval Vieira <sinvalneto01@gmail.com>
2019-07-30 21:21:57 +0200

    Added mutable shared state index

--- src/26/actix_hello_world/src/main.rs
+++ src/26/actix_hello_world/src/main.rs
@@ -1,8 +1,20 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
 // This struct represents state
 struct AppState {
     app_name: String,
+}
+
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+}
+
+fn _index(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {}", counter) // <- response with count
 }
 
 fn index(info: web::Path<(u32, String)>) -> impl Responder {
@@ -20,11 +32,17 @@ fn index3(data: web::Data<AppState>) -> impl Responder {
 }
 
 fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
             .data(AppState {
                 app_name: String::from("world"),
             })
+            .register_data(counter.clone()) // <- register the created data
+            .service(web::resource("/counter").to(_index))
             .service(web::resource("/{id}/{name}/index.html").to(index))
             .service(web::resource("/index2").to(index2))
             .service(web::resource("/index3").to(index3))
