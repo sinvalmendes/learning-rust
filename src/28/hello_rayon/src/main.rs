feac0e38b070ac87d5990bd7ad00f8a23f9809dd
Sinval Vieira <sinvalneto01@gmail.com>
2019-08-12 21:24:03 +0200

    Added parallel version of vec! sum using Rayon

--- src/28/hello_rayon/src/main.rs
+++ src/28/hello_rayon/src/main.rs
@@ -1,10 +1,22 @@
+extern crate rayon;
+
+use rayon::prelude::*;
+
 fn main() {
     println!("Hello, world!");
 
     let input = vec![1, 2, 3, 4, 5];
     let result = sum_array_iterative(input);
-
     assert_eq!(15, result);
+
+    let mut par_input = vec![1, 2, 3, 4, 5];
+    let result = sum_array_parallel(&mut par_input);
+    assert_eq!(15, result);
+}
+
+fn sum_array_parallel(input: &mut Vec<i32>) -> i32 {
+    let result: i32 = input.par_iter_mut().map(|&mut p| p).sum();
+    result
 }
 
 fn sum_array_iterative(input: Vec<u32>) -> u32 {
