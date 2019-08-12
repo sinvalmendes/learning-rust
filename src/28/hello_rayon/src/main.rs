476c4911004dc52d8c1316f4f40cfb96ac1ba81f
Sinval Vieira <sinvalneto01@gmail.com>
2019-08-12 20:51:13 +0200

    Added iterative implementation of vec! sum

--- src/28/hello_rayon/src/main.rs
+++ src/28/hello_rayon/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+
+    let input = vec![1, 2, 3, 4, 5];
+    let result = sum_array_iterative(input);
+
+    assert_eq!(15, result);
+}
+
+fn sum_array_iterative(input: Vec<u32>) -> u32 {
+    let mut counter: u32 = 0;
+    for num in input {
+        counter += num;
+    }
+    counter
 }
