4eb5f0060a8692d08cfeda22894ca6caaf658d3c
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-03 20:32:13 +0200

    Refactoring and commenting println

--- src/problems/15/subsets2/src/main.rs
+++ src/problems/15/subsets2/src/main.rs
@@ -16,7 +16,7 @@
 // ]
 
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
 
     let nums = vec![1, 2, 2];
     let result = Solution::subsets_with_dup(nums);
@@ -40,11 +40,11 @@ struct Solution {}
 
 impl Solution {
     pub fn subsets_with_dup(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        println!("nums {:?}", nums);
+        // println!("nums {:?}", nums);
         let mut result: Vec<Vec<i32>> = vec![vec![]];
 
         Solution::helper(nums, &mut vec![], &mut result, "---");
-        println!("final result => {:?}", result);
+        // println!("final result => {:?}", result);
         return result;
     }
 
@@ -54,15 +54,15 @@ impl Solution {
         result: &mut Vec<Vec<i32>>,
         string: &str,
     ) {
-        println!(
-            "{:?} nums {:?}, combination {:?}, result {:?}",
-            string, nums, combination, result
-        );
+        // println!(
+        //     "{:?} nums {:?}, combination {:?}, result {:?}",
+        //     string, nums, combination, result
+        // );
         if nums.len() == 0 {
-            println!(
-                "{:?} nums.len() == 0, combination {:?}",
-                string, combination
-            );
+            // println!(
+            //     "{:?} nums.len() == 0, combination {:?}",
+            //     string, combination
+            // );
             if result.contains(combination) {
                 return;
             };
@@ -72,26 +72,26 @@ impl Solution {
 
         let mut nums_clone = nums.clone();
         let selected = nums_clone.remove(0);
-        println!("{:?} selected {:?}", string, selected);
+        // println!("{:?} selected {:?}", string, selected);
 
         let new_string = format!("---{}", string);
 
-        let result1 = Solution::helper(
+        Solution::helper(
             nums_clone.clone(),
             &mut combination.clone(),
             result,
             &new_string,
         );
-        println!("{:?} result1 {:?}", string, result1);
+        // println!("{:?} result1 {:?}", string, result1);
 
         combination.push(selected);
-        let result2 = Solution::helper(
+        Solution::helper(
             nums_clone.clone(),
             &mut combination.clone(),
             result,
             &new_string,
         );
-        println!("{:?} result2 {:?}", string, result2);
+        //println!("{:?} result2 {:?}", string, result2);
 
         return;
     }
