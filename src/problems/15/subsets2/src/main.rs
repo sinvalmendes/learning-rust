2165a8b43f4cc7119a1b7444ab2d281daba1beaf
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-03 20:17:34 +0200

    Added brute force implementation without unique solution treatment

--- src/problems/15/subsets2/src/main.rs
+++ src/problems/15/subsets2/src/main.rs
@@ -35,6 +35,54 @@ struct Solution {}
 impl Solution {
     pub fn subsets_with_dup(nums: Vec<i32>) -> Vec<Vec<i32>> {
         println!("nums {:?}", nums);
-        return vec![];
+        let mut result: Vec<Vec<i32>> = vec![vec![]];
+
+        Solution::helper(nums, &mut vec![], &mut result, "---");
+        return result;
+    }
+
+    pub fn helper(
+        nums: Vec<i32>,
+        combination: &mut Vec<i32>,
+        result: &mut Vec<Vec<i32>>,
+        string: &str,
+    ) {
+        println!(
+            "{:?} nums {:?}, combination {:?}, result {:?}",
+            string, nums, combination, result
+        );
+        if nums.len() == 0 {
+            println!(
+                "{:?} nums.len() == 0, combination {:?}",
+                string, combination
+            );
+            result.push(combination.clone());
+            return;
+        }
+
+        let mut nums_clone = nums.clone();
+        let selected = nums_clone.remove(0);
+        println!("{:?} selected {:?}", string, selected);
+
+        let new_string = format!("---{}", string);
+
+        let result1 = Solution::helper(
+            nums_clone.clone(),
+            &mut combination.clone(),
+            result,
+            &new_string,
+        );
+        println!("{:?} result1 {:?}", string, result1);
+
+        combination.push(selected);
+        let result2 = Solution::helper(
+            nums_clone.clone(),
+            &mut combination.clone(),
+            result,
+            &new_string,
+        );
+        println!("{:?} result2 {:?}", string, result2);
+
+        return;
     }
 }
