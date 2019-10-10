54e01b026497ac7eb21802c6dbc42a52dd65780a
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-10 22:03:56 +0200

    Added two basic examples of how lifetimes can be used to help the compiler understand how to manage different scopes

--- src/30/lifetimes/src/main.rs
+++ src/30/lifetimes/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    let x: i32 = 5;
+    let y: i32 = 2;
+
+    let e;
+    {
+        let w = &y;
+        e = helper(&x, w);
+    }
+    // println!("w: {}", w); // w doesn't exist anymore
+    println!("e: {}", e); // but it's value now it's in e
+
+    let i;
+    {
+        let z: &'static i32 = &3;
+        i = helper(&x, z);
+        println!("z: {}", z);
+    }
+    // println!("z: {}", z); // z doesn't exist anymore
+    println!("i: {}", i); // but z value is now in i
+}
+
+fn helper<'a>(x: &'a i32, y: &'a i32) -> &'a i32 {
+    if x < y {
+        x
+    } else {
+        y
+    }
 }
