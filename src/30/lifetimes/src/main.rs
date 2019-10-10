a2b492af2e17e68de6668a0c81ee40d1c5a4842c
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-10 22:10:36 +0200

    Added one more scope block to ilustrate lifetime and death of references

--- src/30/lifetimes/src/main.rs
+++ src/30/lifetimes/src/main.rs
@@ -10,14 +10,17 @@ fn main() {
     // println!("w: {}", w); // w doesn't exist anymore
     println!("e: {}", e); // but it's value now it's in e
 
-    let i;
     {
-        let z: &'static i32 = &3;
-        i = helper(&x, z);
-        println!("z: {}", z);
+        let i;
+        {
+            let z: &'static i32 = &3;
+            i = helper(&x, z);
+            println!("z: {}", z);
+        }
+        // println!("z: {}", z); // z doesn't exist anymore
+        println!("i: {}", i); // but z value is now in i
     }
-    // println!("z: {}", z); // z doesn't exist anymore
-    println!("i: {}", i); // but z value is now in i
+    // println!("i: {}", i); // i doesn't exist anymore
 }
 
 fn helper<'a>(x: &'a i32, y: &'a i32) -> &'a i32 {
