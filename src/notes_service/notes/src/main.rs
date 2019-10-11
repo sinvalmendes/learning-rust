2c907ce9cbae368b783b7c06fe9215a309397556
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-11 23:57:52 +0200

    Added actix-web dependency and removed actix framework for now

--- src/notes_service/notes/src/main.rs
+++ src/notes_service/notes/src/main.rs
@@ -1,6 +1,13 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
+
 fn main() {
     println!("Hello, world!");
-    let system = actix::System::new("test");
-
-    system.run();
 }
