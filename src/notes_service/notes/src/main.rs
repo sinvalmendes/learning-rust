2ed618673babf54950ce4af6e043f08d3cef604a
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-12 23:29:10 +0200

    Added healthcheck endpoint

--- src/notes_service/notes/src/main.rs
+++ src/notes_service/notes/src/main.rs
@@ -4,14 +4,16 @@ fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-fn index2() -> impl Responder {
-    HttpResponse::Ok().body("Hello world again!")
+fn health() -> impl Responder {
+    HttpResponse::Ok().body("{'status': 'ok'}")
 }
 
 fn main() {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
+            .route("/health", web::get().to(health))
+
     })
     .bind("127.0.0.1:8000")
     .expect("Can not bind to port 8000")
