e62071eb9577a137057388ed5c585f889be7c2f8
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-13 16:20:25 +0200

    Added serde dependency and basic Note struct with post endpoint to deserialize JSON value for Note

--- src/notes_service/notes/src/main.rs
+++ src/notes_service/notes/src/main.rs
@@ -1,11 +1,24 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde::{Serialize, Deserialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Note {
+    title: String,
+    content: String,
+}
+
+fn capture_note(note: web::Json<Note>) -> impl Responder {
+    // let new_note = store_in_db(note.timestamp, &note.title, &note.content);
+    // format!("got note {}", new_note.id.unwrap())
+    println!("capture_note: {:?}", note);
+}
 
 fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("Hello world!");
 }
 
 fn health() -> impl Responder {
-    HttpResponse::Ok().body("{'status': 'ok'}")
+    HttpResponse::Ok().body("{'status': 'ok'}");
 }
 
 fn main() {
@@ -13,7 +26,7 @@ fn main() {
         App::new()
             .route("/", web::get().to(index))
             .route("/health", web::get().to(health))
-
+            .route("/notes", web::post().to(capture_note))
     })
     .bind("0.0.0.0:8000")
     .expect("Can not bind to port 8000")
