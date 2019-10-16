d0c1755bc13d84c33f80bb7d770db4cda4f27816
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-16 20:48:01 +0200

    Added some logging calls

--- src/notes_service/notes/src/api.rs
+++ src/notes_service/notes/src/api.rs
@@ -5,16 +5,17 @@ use serde::Deserialize;
 use crate::db;
 
 pub fn create_note(note: web::Json<NewNote>, pool: web::Data<db::PgPool>) -> impl Responder {
-    println!("create_note: {:?}", note);
+    info!("create_note: {:?}", note);
     let new_note = NewNote {
         title: note.title.clone(),
         content: note.content.clone(),
     };
     let result = db::create_note(new_note, &pool);
-    println!("create_note result: {:?}", result)
+    debug!("create_note result: {:?}", result);
 }
 
 pub fn get_all_notes(req: HttpRequest, pool: web::Data<db::PgPool>) -> impl Responder {
+    info!("get_all_notes");
     let result = db::get_all_notes(&pool);
     match result {
         Ok(x) => HttpResponse::Ok().json(x),
@@ -27,6 +28,7 @@ pub fn get_notes_by_title(
     req: HttpRequest,
     pool: web::Data<db::PgPool>,
 ) -> impl Responder {
+    info!("get_notes_by_title: {:?}", params);
     let result = db::select_by_title(params.title.clone(), &pool);
     match result {
         Ok(x) => HttpResponse::Ok().json(x),
@@ -34,17 +36,15 @@ pub fn get_notes_by_title(
     }
 }
 
-#[derive(Deserialize)]
+#[derive(Debug, Deserialize)]
 pub struct TitleGetParams {
     title: String,
 }
 
 pub fn index() -> impl Responder {
-    println!("index");
     HttpResponse::Ok().body("Hello world!");
 }
 
 pub fn health() -> impl Responder {
-    println!("health");
     HttpResponse::Ok().body("{'status': 'ok'}");
 }
