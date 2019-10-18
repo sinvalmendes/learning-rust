85004082ffb73678996e25a94283e59cfdc2af6a
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-18 11:12:41 +0200

    Added a test case with database setup to test get_all_notes endpoints, needs refactoring

--- src/notes_service/notes/src/tests.rs
+++ src/notes_service/notes/src/tests.rs
@@ -1,10 +1,13 @@
 #[cfg(test)]
 mod tests {
-    use crate::api::{health, index};
-
+    use crate::api::{get_all_notes, health, index};
+    use crate::db::*;
     use actix_service::Service;
+    use actix_web::middleware::Logger;
     use actix_web::{http::StatusCode, test, web, App};
     use bytes::Bytes;
+    use dotenv::dotenv;
+    use std::env;
 
     #[test]
     fn test_add() {
@@ -39,5 +42,32 @@ mod tests {
         // println!("{:?}", body.as_ref().unwrap());
         let result = test::read_body(resp);
         assert_eq!(result, Bytes::from_static(b"{\"status\": \"ok\"}"));
+    }
+
+    #[test]
+    fn test_get_all_notes_endpoint() {
+        dotenv().ok();
+
+        env::set_var("RUST_LOG", "debug,actix_todo=debug,actix_web=info");
+        env_logger::init();
+        info!("started!");
+
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let pool = init_pool(&database_url).expect("Failed to create pool");
+
+        let mut app = test::init_service(
+            App::new()
+                .data(pool.clone())
+                .wrap(Logger::default())
+                .service(web::resource("/notes").to(get_all_notes)),
+        );
+
+        // Create request object
+        let req = test::TestRequest::with_uri("/notes").to_request();
+
+        // Execute application
+        let resp = test::block_on(app.call(req)).unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+        // let result = test::read_body(resp);
     }
 }
