9f7f1cd5799c1d21609b3271709224a939b479ba
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-18 16:07:51 +0200

    Added serde_json dependency and also added test create_notes endpoint, which is still not working

--- src/notes_service/notes/src/tests.rs
+++ src/notes_service/notes/src/tests.rs
@@ -1,14 +1,14 @@
 #[cfg(test)]
 mod tests {
-    use crate::api::{get_all_notes, health, index};
+    use crate::api::{create_note, get_all_notes, health, index};
     use crate::db::*;
+    use crate::model::NewNote;
     use actix_service::Service;
     use actix_web::middleware::Logger;
     use actix_web::{http::StatusCode, test, web, App};
     use bytes::Bytes;
     use dotenv::dotenv;
     use std::env;
-
     #[test]
     fn test_add() {
         assert_eq!(4, 2 + 2);
@@ -43,12 +43,47 @@ mod tests {
     }
 
     #[test]
-    fn test_get_all_notes_endpoint() {
+    fn test_create_notes_endpoint() {
         dotenv().ok();
 
         env::set_var("RUST_LOG", "debug,actix_todo=debug,actix_web=info");
         env_logger::init();
-        info!("started!");
+
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let pool = init_pool(&database_url).expect("Failed to create pool");
+
+        let mut app = test::init_service(
+            App::new()
+                .data(pool.clone())
+                .wrap(Logger::default())
+                .service(web::resource("/notes").to(create_note)),
+        );
+
+        let data = NewNote {
+            title: String::from("abc"),
+            content: String::from("bla"),
+        };
+        let serialized = serde_json::to_string(&data).unwrap();
+
+        // Create request object
+        let req = test::TestRequest::post()
+            .set_json(&serialized)
+            .uri("/notes")
+            .to_request();
+
+        // Execute application
+        let resp = test::block_on(app.call(req)).unwrap();
+        let result = test::read_body(resp);
+        println!("{:?}", result);
+        // assert_eq!(resp.status(), StatusCode::OK);
+        // let result = test::read_body(resp);
+    }
+
+    #[test]
+    fn test_get_all_notes_endpoint() {
+        dotenv().ok();
+
+        env::set_var("RUST_LOG", "debug,actix_todo=debug,actix_web=info");
 
         let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
         let pool = init_pool(&database_url).expect("Failed to create pool");
