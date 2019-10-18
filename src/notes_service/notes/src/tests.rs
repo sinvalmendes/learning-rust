fe1c23f041a5891ad27b443ae4ec6bf203925d8a
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-18 10:54:18 +0200

    Added index path unit test

--- src/notes_service/notes/src/tests.rs
+++ src/notes_service/notes/src/tests.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use crate::api::health;
+    use crate::api::{health, index};
 
     use actix_service::Service;
     use actix_web::{http::StatusCode, test, web, App};
@@ -9,6 +9,20 @@ mod tests {
     #[test]
     fn test_add() {
         assert_eq!(4, 2 + 2);
+    }
+
+    #[test]
+    fn test_index_endpoint() {
+        let mut app = test::init_service(App::new().service(web::resource("/").to(index)));
+
+        // Create request object
+        let req = test::TestRequest::with_uri("/").to_request();
+
+        // Execute application
+        let resp = test::block_on(app.call(req)).unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+        let result = test::read_body(resp);
+        assert_eq!(result, Bytes::from_static(b"Hello world!"));
     }
 
     #[test]
