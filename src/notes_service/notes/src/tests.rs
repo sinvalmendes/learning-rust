ad3cd4594424f570db4030f29ead37a11ebaae49
Sinval Vieira <sinvalneto01@gmail.com>
2019-10-19 16:56:03 +0200

    Moved all test cases to new setup-teardown template

--- src/notes_service/notes/src/tests.rs
+++ src/notes_service/notes/src/tests.rs
@@ -54,61 +54,68 @@ mod tests {
 
     #[test]
     fn test_health_endpoint() {
-        let mut app = test::init_service(App::new().service(web::resource("/health").to(health)));
+        run_test(|| {
+            let mut app =
+                test::init_service(App::new().service(web::resource("/health").to(health)));
 
-        // Create request object
-        let req = test::TestRequest::with_uri("/health").to_request();
+            // Create request object
+            let req = test::TestRequest::with_uri("/health").to_request();
 
-        // Execute application
-        let resp = test::block_on(app.call(req)).unwrap();
-        assert_eq!(resp.status(), StatusCode::OK);
-        let result = test::read_body(resp);
-        assert_eq!(result, Bytes::from_static(b"{\"status\": \"ok\"}"));
+            // Execute application
+            let resp = test::block_on(app.call(req)).unwrap();
+            assert_eq!(resp.status(), StatusCode::OK);
+            let result = test::read_body(resp);
+            assert_eq!(result, Bytes::from_static(b"{\"status\": \"ok\"}"));
+        })
     }
 
     #[test]
     fn test_create_notes_endpoint() {
-        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-        let pool = init_pool(&database_url).expect("Failed to create pool");
+        run_test(|| {
+            let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+            let pool = init_pool(&database_url).expect("Failed to create pool");
 
-        let mut app = test::init_service(
-            App::new()
-                .data(pool.clone())
-                .wrap(Logger::default())
-                .service(web::resource("/notes").to(create_note)),
-        );
+            let mut app = test::init_service(
+                App::new()
+                    .data(pool.clone())
+                    .wrap(Logger::default())
+                    .service(web::resource("/notes").to(create_note)),
+            );
 
-        // Create request object
-        let req = test::TestRequest::post()
-            .set_payload(Bytes::from_static(
-                b"{\"title\": \"abc\",\"content\": \"bla\"}",
-            ))
-            .header("Content-Type", "application/json")
-            .uri("/notes")
-            .to_request();
+            // Create request object
+            let req = test::TestRequest::post()
+                .set_payload(Bytes::from_static(
+                    b"{\"title\": \"abc\",\"content\": \"bla\"}",
+                ))
+                .header("Content-Type", "application/json")
+                .uri("/notes")
+                .to_request();
 
-        // Execute application
-        let resp = test::block_on(app.call(req)).unwrap();
-        assert_eq!(resp.status(), StatusCode::OK);
+            // Execute application
+            let resp = test::block_on(app.call(req)).unwrap();
+            assert_eq!(resp.status(), StatusCode::OK);
+        })
     }
 
     #[test]
     fn test_get_all_notes_endpoint() {
-        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-        let pool = init_pool(&database_url).expect("Failed to create pool");
+        run_test(|| {
+            let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+            let pool = init_pool(&database_url).expect("Failed to create pool");
 
-        let mut app = test::init_service(
-            App::new()
-                .data(pool.clone())
-                .wrap(Logger::default())
-                .service(web::resource("/notes").to(get_all_notes)),
-        );
+            let mut app = test::init_service(
+                App::new()
+                    .data(pool.clone())
+                    .wrap(Logger::default())
+                    .service(web::resource("/notes").to(get_all_notes)),
+            );
 
-        // Create request object
-        let req = test::TestRequest::with_uri("/notes").to_request();
+            // Create request object
+            let req = test::TestRequest::with_uri("/notes").to_request();
 
-        // Execute application
-        let resp = test::block_on(app.call(req)).unwrap();
-        assert_eq!(resp.status(), StatusCode::OK);
+            // Execute application
+            let resp = test::block_on(app.call(req)).unwrap();
+            assert_eq!(resp.status(), StatusCode::OK);
+        })
     }
 }
